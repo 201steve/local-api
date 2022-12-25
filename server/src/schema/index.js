badd8b799eb7c4e546a589838b8e97019e1dc7cd
201steve <jeongillee201@gmail.com>
2022-12-25 19:29:43 +0900

    Graphql 환경세팅 및 schema 정의

---
+++ server/src/schema/index.js
@@ -0,0 +1,14 @@
+import { gql } from "apollo-server-express";
+import messageSchema from "./messages";
+import userSchema from "./user";
+
+const linkSchema = gql`
+  type Query{
+    :Boolean    
+  }
+  type Mutation {
+    :Boolean
+
+  }
+`;
+export default [linkSchema, messageSchema, userSchema];
