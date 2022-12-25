d02ab4767dbae23cc1f6ed08f3697b6c5a37d781
201steve <jeongillee201@gmail.com>
2022-12-25 15:46:23 +0900

    Rest API 통신 기능구현, 추가 예외처리까지

--- client/components/MsgList.js
+++ client/components/MsgList.js
@@ -1,42 +1,39 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import MsgItem from "./MsgItem";
 import MsgInput from "./MsgInput";
 import fetcher from "../pages/fetcher";
 
-const UserIds = ["roy", "jay"];
-const getRandomUserId = () => UserIds[Math.round(Math.random())];
-
 const MsgList = () => {
+  const {
+    query: { userId = "" },
+  } = useRouter();
   const [msgs, setMsgs] = useState([]);
-  console.log(msgs);
   const [editingId, setEditingId] = useState(null);
-  const onCreate = (text) => {
-    const newMsg = {
-      id: msgs.length + 1,
-      userId: getRandomUserId(),
-      timestamp: Date.now(),
-      text: `${msgs.length + 1} ${text}`,
-    };
+
+  const onCreate = async (text) => {
+    const newMsg = await fetcher("post", "/messages", { text, userId });
+    if (!newMsg) throw Error("something wrong");
     setMsgs((msgs) => [newMsg, ...msgs]);
   };
 
-  const onUpdate = (text, id) => {
+  const onUpdate = async (text, id) => {
+    const newMsg = await fetcher("put", `/messages/${id}`, { text, userId });
+    if (!newMsg) throw Error("something wrong");
     setMsgs((msgs) => {
       const targetIndex = msgs.findIndex((msg) => msg.id === id);
       if (targetIndex < 0) return msgs;
       const newMsgs = [...msgs];
-      newMsgs.splice(targetIndex, 1, {
-        ...msgs[targetIndex],
-        text,
-      });
+      newMsgs.splice(targetIndex, 1, newMsg);
       return newMsgs;
     });
     doneEdit();
   };
 
-  const onDelete = (id) => {
+  const onDelete = async (id) => {
+    const receivedId = await fetcher("delete", `/messages/${id}`, { params: { userId } });
     setMsgs((msgs) => {
-      const targetIndex = msgs.findIndex((msg) => msg.id === id);
+      const targetIndex = msgs.findIndex((msg) => msg.id === receivedId + "");
       if (targetIndex < 0) return msgs;
       const newMsgs = [...msgs];
       newMsgs.splice(targetIndex, 1);
@@ -56,7 +53,7 @@ const MsgList = () => {
   }, []);
   return (
     <>
-      <MsgInput mutate={onCreate} />
+      {userId && <MsgInput mutate={onCreate} />}
       <ul className="messages">
         {msgs.map((x) => (
           <MsgItem
@@ -66,6 +63,7 @@ const MsgList = () => {
             onDelete={() => onDelete(x.id)}
             startEdit={() => setEditingId(x.id)}
             isEditing={editingId === x.id}
+            myId={userId}
           />
         ))}
       </ul>
